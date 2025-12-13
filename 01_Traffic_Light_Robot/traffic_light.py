38cb0018df5e4bb2cbc3c67a436c6505c1b5a4f1
RoboticsBySaira <minsasajjad16@gmail.com>
2025-12-13 22:26:00 +0500

    Fix indentation in traffic_light.py

--- 01_Traffic_Light_Robot/traffic_light.py
+++ 01_Traffic_Light_Robot/traffic_light.py
@@ -1,10 +1,9 @@
 light = input("Enter traffic light color (red / yellow / green): ").lower()
-
 if light == "green":
-    print("Robot moves forward")
+   print("Robot moves forward")
 elif light == "yellow":
-    print("Robot slows down")
+   print("Robot slows down")
 elif light == "red":
-    print("Robot stops")
+   print("Robot stops")
 else:
-    print("Invalid signal")
+   print("Invalid signal")
